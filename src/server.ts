176d75c816596a2b68c134e63388c443d4f687a1
Wolly <golliwogg@gmx.de>
2021-05-07 18:36:38 +0200

    testing jenkins

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send({ message: 'test' })
+    res.send({ message: 'test jenkins' })
 })
 
 app.post('/create-game', (req, res) => {
