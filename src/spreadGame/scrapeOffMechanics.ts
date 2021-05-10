717aea67ba102121e06a3d4258906f02a3bf6573
Wolly <golliwogg@gmx.de>
2021-05-10 18:07:27 +0200

    fixed scrapeoff bugs

--- src/spreadGame/scrapeOffMechanics.ts
+++ src/spreadGame/scrapeOffMechanics.ts
@@ -4,13 +4,15 @@ import { distanceToBoundary } from '../shared/game/map'
 import basicMechanics from './basicMechanics'
 import Bubble from './bubble'
 import Cell from './cell'
-import { SpreadGameMechanics, FightModifier } from './spreadGame'
+import { FightModifier, SpreadGameMechanics } from './spreadGame'
+
+const minOverlap = 5
 
 const scrapeOffMechanics: SpreadGameMechanics = {
     collides: (bubble: Bubble, entity: Bubble | Cell) => {
         return (
             distance(bubble.position, entity.position) <=
-            bubble.radius + entity.radius
+            bubble.radius + entity.radius - minOverlap
         )
     },
     collideBubble: (
@@ -23,17 +25,25 @@ const scrapeOffMechanics: SpreadGameMechanics = {
         const unitDiff = bubble1.units - bubble2.units
         const maxUnits = radiusToUnits(dist)
         if (unitDiff >= maxUnits) {
-            bubble1.units = ((unitDiff + maxUnits) / (2 * dist)) ** 2
+            bubble1.units =
+                ((unitDiff + maxUnits) / (2 * dist)) ** 2 *
+                radiusToUnitsFixPoint
             bubble1.updateRadius()
             return [bubble1, null]
         } else if (unitDiff <= -maxUnits) {
-            bubble2.units = ((-unitDiff + maxUnits) / (2 * dist)) ** 2
+            bubble2.units =
+                ((-unitDiff + maxUnits) / (2 * dist)) ** 2 *
+                radiusToUnitsFixPoint
             bubble2.updateRadius()
-            return [bubble2, null]
+            return [null, bubble2]
         } else {
-            bubble1.units = ((unitDiff + maxUnits) / (2 * dist)) ** 2
+            bubble1.units =
+                ((unitDiff + maxUnits) / (2 * dist)) ** 2 *
+                radiusToUnitsFixPoint
             bubble1.updateRadius()
-            bubble2.units = ((-unitDiff + maxUnits) / (2 * dist)) ** 2
+            bubble2.units =
+                ((-unitDiff + maxUnits) / (2 * dist)) ** 2 *
+                radiusToUnitsFixPoint
             bubble2.updateRadius()
             return [bubble1, bubble2]
         }
